fbcdba7f66773caa8ee6327b66f48d9b53f1f371
Nathan <nathan.froeh@gmail.com>
2019-09-15 12:10:17 -0600

    Add Form component

---
+++ src/components/Form.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="Form">
+    <form action="">
+      <input type="text">
+      <input type="submit">
+    </form>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Form'
+}
+</script>
