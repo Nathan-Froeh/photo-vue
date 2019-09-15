053cce45866407c131f644e5d7b13170ac8dc4ea
Nathan <nathan.froeh@gmail.com>
2019-09-15 12:13:49 -0600

    Add form styles

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -1,8 +1,8 @@
 <template>
   <div id="Form">
     <form action="">
-      <input type="text">
-      <input type="submit">
+      <input type="text" placeholder="Search for images">
+      <input type="submit" value="Search">
     </form>
 
   </div>
@@ -13,3 +13,9 @@ export default {
   name: 'Form'
 }
 </script>
+
+<style scoped>
+  input {
+    border-radius: 10px
+  }
+</style>
