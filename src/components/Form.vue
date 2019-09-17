a822e29eba1ad2b4f4aed776d7b14cf319542461
Nathan <nathan.froeh@gmail.com>
2019-09-16 20:21:35 -0600

    Add input transparency

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -6,8 +6,9 @@
         placeholder="Search for images"
         v-on:change="changeSearch"  
         :value="searchType"
+        class='search-box'
       >
-      <input type="submit" value="Search">
+      <input type="submit" value="Search" class='submit-button'>
     </form>
 
   </div>
@@ -24,12 +25,28 @@ export default {
   #Form {
     position: absolute;
     z-index: 10;
-    margin-top: 10%;
+    margin-top: 5%;
     width: 100%;
     display: flex;
     justify-content: center;
   }
   input {
-    border-radius: 10px;
+    border-radius: 15px;
+    height: 35px;
+    font-size: 18px;
+    background-color: rgba(255, 255, 255, .3);
+    margin: 10px;
+    /* box-shadow: inset 3px 3px 3px 3px rgba(0, 0, 0, .4); */
+    border: solid #D8D8D8 1px;
   }
+  .search-box {
+    width: 260px;
+    padding-left: 10px;
+  }
+  .submit-button {
+    width: 100px;
+  }
+  ::-webkit-input-placeholder { 
+  color: black;
+}
 </style>
